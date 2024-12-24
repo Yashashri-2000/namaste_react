d804827bba06a4ab207548722427a58cceacc715
Yashashri Firake <yashashrifirake@gmail.com>
2024-12-24 21:45:04 +0530

    episode_02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am a h1 tag!"),
@@ -8,11 +11,6 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "Im a h2 tag"),
   ]),
 ]);
-console.log("parents",parent)
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello From App js"
-);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste React 
+#Namaste React
+
+#parcel
+-Dev build
+-local server
+-HMR- Hot Module Replacement
+-File Watching Algorithm- written in c++
+-Caching- Faster Builds
+-Image Optimazation
+-Minification of our file
+-Bundling
+-Compress
+-Consistent Hashing
+-code splitting
+-Differential Bundling- support older browsers
+-Dianostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bumdles
